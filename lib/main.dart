20010ae7af49a45b8d43c522c404d119c34ead43
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-01-04 15:46:48 +0300

    build the wifi button manually

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,150 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Sleepy',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  Color mainColor = const Color(0XFF2F9EEF);
+  bool isTapped = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        title: const Text(
+          'Sleepy',
+          style: TextStyle(
+            color: Color(0XFF2F9EEF),
+            fontSize: 24,
+          ),
         ),
+        leading: Icon(
+          Icons.menu,
+          color: mainColor,
+          size: 27,
+        ),
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        elevation: 0,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: Column(
+        children: [
+          // Row 1: Text
+          Container(
+            padding: const EdgeInsets.all(10),
+            // color: Colors.amber,
+            margin: const EdgeInsets.only(top: 10),
+            child: Align(
+              // alignment: Alignment.center,
+              child: Text(
+                'Choose what to do on sleep',
+                style: TextStyle(
+                  color: Color(0XFF2F9EEF),
+                  fontSize: 15,
+                  fontWeight: FontWeight.w500,
+                ),
+                // textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          // Row 2: The Icons
+          Container(
+            // color: Colors.amber,
+            margin: const EdgeInsets.only(top: 10),
+            padding: const EdgeInsets.all(8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(children: [
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        isTapped ? isTapped = false : isTapped = true;
+                      });
+                      print(isTapped);
+                      print("Tapped the wifi container");
+                    },
+                    child: Container(
+                        width: 86,
+                        height: 86,
+                        decoration: BoxDecoration(
+                          color: mainColor,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: isTapped
+                            ? Icon(
+                                Icons.wifi_rounded,
+                                size: 62,
+                                color: Colors.white,
+                              )
+                            : Icon(
+                                Icons.wifi_off_rounded,
+                                size: 62,
+                                color: Colors.white,
+                              )),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 8),
+                    child: Text(
+                      'Wi-Fi',
+                      style: TextStyle(
+                        color: mainColor,
+                        fontSize: 14,
+                      ),
+                    ),
+                  ),
+                ]),
+                Column(children: [
+                  Icon(
+                    Icons.wifi,
+                    size: 86,
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 8),
+                    child: Text('Turn off Wi-Fi'),
+                  ),
+                ]),
+                Column(children: [
+                  Icon(
+                    Icons.wifi,
+                    size: 86,
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 8),
+                    child: Text('Turn off Wi-Fi'),
+                  ),
+                ]),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
 }
+
+

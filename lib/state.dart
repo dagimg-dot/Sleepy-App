c37559e677821a5b4a373f7e58ac8470aff48fd9
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-03-01 23:11:21 +0300

    add code to track state change using provider

--- lib/state.dart
+++ lib/state.dart
@@ -4,16 +4,33 @@ import 'package:flutter/foundation.dart';
 
 class TogglesModel extends ChangeNotifier {
   bool isWifiButtonOff = false;
+  bool isBluetoothButtonOff = false;
+  bool isScreenOff = false;
+  bool isDoNotDisturbModeOff = false;
 
-  // isWifiButtonOff get => isWifiButtonOff;
-  // isWifiButtonOff get => isWifiButtonOff;
-
-  bool get getState {
-    return isWifiButtonOff;
-  }
-
-  toggle() {
+  void toggleWifiButton() {
     isWifiButtonOff = !isWifiButtonOff;
-    notifyListeners();  
+    notifyListeners();
   }
+
+  void toggleBluetoothButton() {
+    isBluetoothButtonOff = !isBluetoothButtonOff;
+    notifyListeners();
+  }
+
+  void toggleDoNotDisturbButton() {
+    isScreenOff = !isScreenOff;
+    notifyListeners();
+  }
+
+  void toggleAirplaneModeButton() {
+    isDoNotDisturbModeOff = !isDoNotDisturbModeOff;
+    notifyListeners();
+  }
+
+  bool get getWifiButtonState => isWifiButtonOff;  
+  bool get getBluetoothButtonState => isBluetoothButtonOff;  
+  bool get getScreenButtonState => isScreenOff;
+  bool get getDoNotDisturbButtonState => isDoNotDisturbModeOff; 
+  
 }

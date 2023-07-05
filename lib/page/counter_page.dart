03d40791139fbb316078fabc4f399a090c4e75a6
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-07-05 20:54:30 +0300

    create sample page for trial

---
+++ lib/page/counter_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CounterPage extends StatefulWidget {
+  const CounterPage({super.key});
+
+  @override
+  State<CounterPage> createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter Page'),
+      ),
+      body: const Center(
+        child: Text(
+          'Counter Page',
+        ),
+      ),
+    );
+  }
+}

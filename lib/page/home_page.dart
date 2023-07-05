cf9279e30b4ae82c94ad589fcc16cea5246dc605
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-07-05 20:54:11 +0300

    add routing and unfocusing keyboard with a tap

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -2,12 +2,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:sleepy_app/page/counter_page.dart';
 import 'package:sleepy_app/provider/toggles_model.dart';
 import 'package:sleepy_app/widget/icon_button_widget.dart';
 import 'package:sleepy_app/widget/input_field_widget.dart';
 import 'package:sleepy_app/widget/minute_button_widget.dart';
 
 import '../provider/input_field_model.dart';
+import '../widget/button_widget.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -18,26 +20,40 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   Color mainColor = const Color(0XFF2F9EEF);
+  final controller = TextEditingController();
 
+  @override
+  void initState() {
+    super.initState();
+    controller.addListener(() => setState(() {}));
+  }
 
-  // void startCountdown() {
-  //   setState(() {
-  //     if (numberController.text.isNotEmpty) {
-  //       // time = DateTime(0, 0, 0, 0, int.parse(numberController.text), 0);
-  //       // change the time to minutes and seconds
-  //       Timer.periodic(const Duration(seconds: 1), (timer) {
-  //         setState(() {
-  //           // if (time.second > 0) {
-  //             // time = time.subtract(Duration(seconds: 1));
-  //             print(time);
-  //           // } else {
-  //           //   timer.cancel();
-  //           // }
-  //         });
-  //       });
-  //     }
-  //   });
-  // }
+  void navigateToCounterPage() {
+    if (controller.text.isEmpty) {
+      SnackBar snackBar = const SnackBar(
+        content: Text(
+          'Please enter a number',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 15,
+          ),
+        ),
+        duration: Duration(seconds: 2),
+        backgroundColor: Color.fromARGB(255, 216, 81, 57),
+        behavior: SnackBarBehavior.floating,
+      );
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    } else {
+      Provider.of<InputFieldModel>(context, listen: false)
+          .updateInputFieldText(controller.text);
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const CounterPage(),
+        ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -52,7 +68,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ),
         leading: IconButton(
-            // show a pop up that says i am tapped when tapped
+            // TODO: Implement menu
             tooltip: 'Menu',
             onPressed: () {
               showDialog(
@@ -77,7 +93,7 @@ class _MyHomePageState extends State<MyHomePage> {
           IconButton(
             tooltip: 'Dark Mode',
             onPressed: () {
-              // show a pop up that says i am tapped when tapped
+              // TODO: Implement dark mode
               showDialog(
                 context: context,
                 builder: (context) {
@@ -96,206 +112,188 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
-      body: SingleChildScrollView(
-        physics: const BouncingScrollPhysics(),
-        child: Column(
-          children: [
-            // Row 1: Text
-            Container(
-              padding: const EdgeInsets.all(10),
-              // color: Colors.amber,
-              margin: const EdgeInsets.only(top: 3),
-              child: const Align(
-                // alignment: Alignment.center,
-                child: Text(
-                  'Choose what to do on sleep',
-                  style: TextStyle(
-                    color: Color(0XFF2F9EEF),
-                    fontSize: 15,
-                    fontWeight: FontWeight.w500,
-                  ),
-                  // textAlign: TextAlign.center,
-                ),
-              ),
-            ),
-            // Row 2: The Icons
-            Consumer<TogglesModel>(
-              builder: (context, togglesModel, child) {
-                return Container(
-                  // color: Colors.amber,
-                  margin: const EdgeInsets.only(top: 10),
-                  padding: const EdgeInsets.all(8),
-                  child: Wrap(
-                    spacing: 30,
-                    runSpacing: 20,
-                    alignment: WrapAlignment.spaceAround,
-                    children: [
-                      // Wi-Fi
-                      IconButtonWidget(
-                          label: 'Wi-Fi',
-                          boolState:
-                              Provider.of<TogglesModel>(context, listen: false)
-                                  .getWifiButtonState,
-                          iconON: Icons.wifi_rounded,
-                          iconOFF: Icons.wifi_off_rounded,
-                          onPressed: () => {
-                                Provider.of<TogglesModel>(context,
-                                        listen: false)
-                                    .toggleWifiButton(),
-                              }),
-                      // Bluetooth
-                      IconButtonWidget(
-                          label: 'Bluetooth',
-                          boolState:
-                              Provider.of<TogglesModel>(context, listen: false)
-                                  .getBluetoothButtonState,
-                          iconON: Icons.bluetooth_rounded,
-                          iconOFF: Icons.bluetooth_disabled_rounded,
-                          onPressed: () => {
-                                Provider.of<TogglesModel>(context,
-                                        listen: false)
-                                    .toggleBluetoothButton(),
-                              }),
-                      // Screen
-                      IconButtonWidget(
-                          label: 'Screen',
-                          boolState:
-                              Provider.of<TogglesModel>(context, listen: false)
-                                  .getScreenButtonState,
-                          iconON: Icons.phone_android_rounded,
-                          iconOFF: Icons.mobile_off_rounded,
-                          onPressed: () => {
-                                Provider.of<TogglesModel>(context,
-                                        listen: false)
-                                    .toggleScreenOffButton(),
-                              }),
-                      // SoundMode
-                      IconButtonWidget(
-                          label: 'Sound Mode',
-                          boolState:
-                              Provider.of<TogglesModel>(context, listen: false)
-                                  .getDoNotDisturbButtonState,
-                          iconON: Icons.volume_up_rounded,
-                          iconOFF: Icons.volume_off_rounded,
-                          onPressed: () => {
-                                Provider.of<TogglesModel>(context,
-                                        listen: false)
-                                    .toggleDoNotDisturbButton(),
-                              }),
-                    ],
-                  ),
-                );
-              },
-            ),
-            // Row 3: Counter Box
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text("Timer Started",
+      body: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Column(
+            children: [
+              // Row 1: Text
+              Container(
+                padding: const EdgeInsets.all(10),
+                // color: Colors.amber,
+                margin: const EdgeInsets.only(top: 3),
+                child: const Align(
+                  // alignment: Alignment.center,
+                  child: Text(
+                    'Choose what to do on sleep',
                     style: TextStyle(
                       color: Color(0XFF2F9EEF),
                       fontSize: 15,
-                      fontWeight: FontWeight.w400,
-                    )),
-                Container(
-                  margin: const EdgeInsets.only(left: 10),
-                  padding: const EdgeInsets.all(3),
-                  decoration: BoxDecoration(
-                    color: mainColor,
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                  child: const Text(
-                    // '${time.minute}:${time.second.toString().padLeft(2, '0')}',
-                    '00:00',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 15,
+                      fontWeight: FontWeight.w500,
                     ),
                   ),
                 ),
-              ],
-            ),
-            // Row 4: Add Button
-            Container(
+              ),
+              // Row 2: The Icons
+              Consumer<TogglesModel>(
+                builder: (context, togglesModel, child) {
+                  return Container(
+                    margin: const EdgeInsets.only(top: 10),
+                    padding: const EdgeInsets.all(8),
+                    child: Wrap(
+                      spacing: 30,
+                      runSpacing: 20,
+                      alignment: WrapAlignment.spaceAround,
+                      children: [
+                        // Wi-Fi
+                        IconButtonWidget(
+                            label: 'Wi-Fi',
+                            boolState: Provider.of<TogglesModel>(context,
+                                    listen: false)
+                                .getWifiButtonState,
+                            iconON: Icons.wifi_rounded,
+                            iconOFF: Icons.wifi_off_rounded,
+                            onPressed: () => {
+                                  Provider.of<TogglesModel>(context,
+                                          listen: false)
+                                      .toggleWifiButton(),
+                                }),
+                        // Bluetooth
+                        IconButtonWidget(
+                            label: 'Bluetooth',
+                            boolState: Provider.of<TogglesModel>(context,
+                                    listen: false)
+                                .getBluetoothButtonState,
+                            iconON: Icons.bluetooth_rounded,
+                            iconOFF: Icons.bluetooth_disabled_rounded,
+                            onPressed: () => {
+                                  Provider.of<TogglesModel>(context,
+                                          listen: false)
+                                      .toggleBluetoothButton(),
+                                }),
+                        // Screen
+                        IconButtonWidget(
+                            label: 'Screen',
+                            boolState: Provider.of<TogglesModel>(context,
+                                    listen: false)
+                                .getScreenButtonState,
+                            iconON: Icons.phone_android_rounded,
+                            iconOFF: Icons.mobile_off_rounded,
+                            onPressed: () => {
+                                  Provider.of<TogglesModel>(context,
+                                          listen: false)
+                                      .toggleScreenOffButton(),
+                                }),
+                        // SoundMode
+                        IconButtonWidget(
+                            label: 'Sound Mode',
+                            boolState: Provider.of<TogglesModel>(context,
+                                    listen: false)
+                                .getDoNotDisturbButtonState,
+                            iconON: Icons.volume_up_rounded,
+                            iconOFF: Icons.volume_off_rounded,
+                            onPressed: () => {
+                                  Provider.of<TogglesModel>(context,
+                                          listen: false)
+                                      .toggleDoNotDisturbButton(),
+                                }),
+                      ],
+                    ),
+                  );
+                },
+              ),
+              // Row 3: Counter Box
+              // Row(
+              //   mainAxisAlignment: MainAxisAlignment.center,
+              //   children: [
+              //     const Text("Timer Started",
+              //         style: TextStyle(
+              //           color: Color(0XFF2F9EEF),
+              //           fontSize: 15,
+              //           fontWeight: FontWeight.w400,
+              //         )),
+              //     Container(
+              //       margin: const EdgeInsets.only(left: 10),
+              //       padding: const EdgeInsets.all(3),
+              //       decoration: BoxDecoration(
+              //         color: mainColor,
+              //         borderRadius: BorderRadius.circular(5),
+              //       ),
+              //       child: const Text(
+              //         // '${time.minute}:${time.second.toString().padLeft(2, '0')}',
+              //         '00:00',
+              //         style: TextStyle(
+              //           color: Colors.white,
+              //           fontSize: 15,
+              //         ),
+              //       ),
+              //     ),
+              //   ],
+              // ),
+              // // Row 4: Add Button
+              // Container(
+              //   margin: const EdgeInsets.only(top: 10),
+              //   child: InkWell(
+              //     onTap: () {},
+              //     child: Container(
+              //       width: 20,
+              //       height: 20,
+              //       decoration: BoxDecoration(
+              //         color: mainColor,
+              //         borderRadius: BorderRadius.circular(10),
+              //       ),
+              //       child: const Center(
+              //         child: Text(
+              //           '+',
+              //           style: TextStyle(
+              //             color: Colors.white,
+              //             fontSize: 15,
+              //           ),
+              //         ),
+              //       ),
+              //     ),
+              //   ),
+              // ),
+              // Row 5: Input Box
+              Container(
                 margin: const EdgeInsets.only(top: 10),
+                padding: const EdgeInsets.all(8),
+                child: InputFieldWidget(controller: controller),
+              ),
+              // Row 6: Common minutes
+              Consumer<InputFieldModel>(
+                builder: (context, inputFieldModel, child) {
+                  return Container(
+                    margin: const EdgeInsets.only(top: 10),
+                    padding: const EdgeInsets.all(8),
+                    child: Wrap(
+                      spacing: 15,
+                      runSpacing: 20,
+                      alignment: WrapAlignment.spaceAround,
+                      children: [
+                        MinuteButtonWidget(label: '5', controller: controller),
+                        MinuteButtonWidget(label: '10', controller: controller),
+                        MinuteButtonWidget(label: '15', controller: controller),
+                        MinuteButtonWidget(label: '30', controller: controller),
+                      ],
+                    ),
+                  );
+                },
+              ),
+              // Row 7: Start Button
+              Container(
+                margin: const EdgeInsets.only(top: 17),
                 child: InkWell(
-                  onTap: () {},
-                  child: Container(
-                    width: 20,
-                    height: 20,
-                    decoration: BoxDecoration(
-                      color: mainColor,
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: const Center(
-                      child: Text(
-                        '+',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 15,
-                        ),
-                      ),
-                    ),
-                  ),
-                )),
-            // Row 5: Input Box
-            Container(
-              margin: const EdgeInsets.only(top: 10),
-              padding: const EdgeInsets.all(8),
-              child: const InputFieldWidget(),
-            ),
-            // Row 6: Common minutes
-            Consumer<InputFieldModel>(
-              builder: (context, inputFieldModel, child) {
-                return Container(
-                  // color: Colors.amber,
-                  margin: const EdgeInsets.only(top: 10),
-                  padding: const EdgeInsets.all(8),
-                  child: Wrap(
-                    spacing: 15,
-                    runSpacing: 20,
-                    alignment: WrapAlignment.spaceAround,
-                    children: const [
-                      MinuteButtonWidget(label: '5'),
-                      MinuteButtonWidget(label: '10'),
-                      MinuteButtonWidget(label: '15'),
-                      MinuteButtonWidget(label: '30'),
-                    ],
-                  ),
-                );
-              },
-            ),
-            // Row 7: Start Button
-            // Container(
-            //     margin: const EdgeInsets.only(top: 13),
-            //     child: InkWell(
-            //       splashColor: Colors.amberAccent,
-            //       onTap: () {
-            //         Provider.of<InputFieldModel>(context, listen: false)
-            //             .updateInputFieldText(numberController.text);
-            //         print(Provider.of<InputFieldModel>(context, listen: false)
-            //             .getInputFieldText);
-            //         startCountdown();
-            //       },
-            //       child: Container(
-            //         width: 170,
-            //         height: 45,
-            //         decoration: BoxDecoration(
-            //           color: mainColor,
-            //           borderRadius: BorderRadius.circular(30),
-            //         ),
-            //         child: const Center(
-            //           child: Text(
-            //             'Start',
-            //             style: TextStyle(
-            //               color: Colors.white,
-            //               fontSize: 18,
-            //             ),
-            //           ),
-            //         ),
-            //       ),
-            //     )),
-          ],
+                  onTap: () {
+                    navigateToCounterPage();
+                  },
+                  child: ButtonWidget(
+                      mainColor: mainColor, width: 170, label: 'Start'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );

5c2445616d4d3d18ab109dd3bb1e8c3174f42453
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-07-07 21:59:08 +0300

    impelement DND control functionality

--- lib/services/android_controls.dart
+++ lib/services/android_controls.dart
@@ -1,6 +1,11 @@
+import 'dart:async';
+
 import 'package:android_flutter_wifi/android_flutter_wifi.dart';
 import 'package:device_policy_manager/device_policy_manager.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+import 'package:flutter_dnd/flutter_dnd.dart';
+import 'resume_observer.dart';
 
 class AndroidControls {
   static void toggleWifiButton() {
@@ -10,15 +15,14 @@ class AndroidControls {
 
   static void toggleBT() {
     FlutterBluetoothSerial.instance.requestDisable().then((value) => {
-      if(value == true) {
-        print("Bluetooth is disabled")
-      }
-    });
+          if (value == true) {print("Bluetooth is disabled")}
+        });
   }
 
-  static Future<bool?> requestPermession(context) async {
-    bool isGranted = await DevicePolicyManager.requestPermession("Sleepy needs permission to lock your screen"); 
-    if(isGranted) {
+  static Future<bool?> requestPermessionLS(context) async {
+    bool isGranted = await DevicePolicyManager.requestPermession(
+        "Sleepy needs permission to lock your screen");
+    if (isGranted) {
       return true;
     } else {
       return false;
@@ -27,5 +31,38 @@ class AndroidControls {
 
   static void lockScreen() {
     DevicePolicyManager.lockNow();
+  }
+
+  static Future<bool?> requestPermessionDND(context) async {
+    bool? isGranted = await FlutterDnd.isNotificationPolicyAccessGranted;
+    if (isGranted == true) {
+      return true;
+    } else {
+      await navigateToPolicySettingsAndHandleResult(context);
+      if (await FlutterDnd.isNotificationPolicyAccessGranted == false) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+  }
+
+  static Future<void> navigateToPolicySettingsAndHandleResult(
+      BuildContext context) {
+    final completer = Completer<void>();
+
+    // Navigate to policy settings
+    FlutterDnd.gotoPolicySettings();
+
+    // Listen for app resume event
+    WidgetsBinding.instance.addObserver(
+      ResumeObserver(completer, context),
+    );
+
+    return completer.future;
+  }
+
+  static void toggleDND() {
+    FlutterDnd.setInterruptionFilter(FlutterDnd.INTERRUPTION_FILTER_NONE);
   }
 }
